7e07d74e391195db9987da1129dc1a77d0d99196
John Arocha <john.arocha@clover.com>
2024-10-29 12:36:47 -0600

    added 2 classes

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,10 @@ const Scooter = require('./Scooter')
 
 class ScooterApp {
   // ScooterApp code here
+  constructor() {
+    this.stations = {houston: [], denver: [], coloradoSprings: []}
+    this.registeredUsers = {}
+  }
 }
 
 module.exports = ScooterApp
